6a12ad78ab05493c58fc806194d6ae7cf176f980
Gleyson Carvalho <gleyson_10@hotmail.com>
2020-04-28 22:28:06 -0300

    first commit

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333",
+    secondary: "#ACBED8",
+
+    background: "#222",
+    text: "#f5f5f5",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#D78521",
+    secondary: "#ACBED8",
+
+    background: "#f5f5f5",
+    text: "#333",
+  },
+};
